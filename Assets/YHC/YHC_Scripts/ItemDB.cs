910d2715cd376c5fb0ad449df7429b404725d74b
yhc0607 <121930484+yhc0607@users.noreply.github.com>
2024-03-13 14:40:14 +0900

    후레쉬 추가
    후레쉬 추가

--- Assets/YHC/YHC_Scripts/ItemDB.cs
+++ Assets/YHC/YHC_Scripts/ItemDB.cs
@@ -1,16 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public enum ItemType
+{
+    Scrap,
+    Equipment,
+}
 
 [CreateAssetMenu(fileName = "ItemDB", menuName = "Scriptable Object/ItemDB", order = int.MaxValue)]
-public class ItemDB: ScriptableObject
+public class ItemDB : ScriptableObject
 {
     [Header("아이템 데이터베이스")]
     public string itemName = "_";
-    public float weight;
-    public int price;
-    public GameObject itemModel;
-    public Sprite itemIcon;
     public string itemText = "아이템 정보";
-
+    public float weight = 0;
+    public uint price = 0;
+    public uint stackCount = 1;
+    public float battery = 0;
+    public Sprite itemIcon;
+    public GameObject itemModel;
+    public ItemType itemType;
 }
---
+++ Assets/YHC/YHC_Scripts/Tool.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ItemDB", menuName = "Scriptable Object/ItemDB/Tools", order = 1)]
+public class Tool : Equipment
+{
+}
