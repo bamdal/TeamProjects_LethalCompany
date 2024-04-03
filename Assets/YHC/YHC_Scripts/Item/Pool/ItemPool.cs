131aafd1f7bc51628ce0a5f1d4c30230a7ccfd4f
yhc0607 <121930484+yhc0607@users.noreply.github.com>
2024-04-03 14:50:46 +0900

    후레쉬 프리팹 트랜스폼 초기화

--- Assets/YHC/YHC_Scripts/Item/Pool/ItemPool.cs
+++ Assets/YHC/YHC_Scripts/Item/Pool/ItemPool.cs
@@ -5,10 +5,4 @@ using UnityEngine;
 public class ItemPool : ObjectPool<ItemBase>
 {
     public ItemDB[] itemDBs;
-
-    protected override void GenerateRecycleObjects(int startIndex, int endIndex, ItemBase[] expandedPool)
-    {
-        recycleObject = itemDBs[(int)ItemCode.FlashLight_0].itemPrefab;
-        base.GenerateRecycleObjects(startIndex, endIndex, expandedPool);
-    }
 }
