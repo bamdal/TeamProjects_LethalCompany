26ee84c18a96920157e16cd041f72c0daa101f91
Merongba <jka3200@naver.com>
2024-04-15 13:03:37 +0900

    .

---
+++ Assets/HMC/Enemy.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : EnemyBase
+{
+    /**public float MoveSpeed = 1.0f;
+    public float HP = 10f;
+    //public int currentSpwanCount = 3;
+    public int maxSpawnCount = 3;
+    public void Update() 
+    {
+        if(currentSpwanCount < maxSpawnCount)
+        {
+            SpawnMonsters();
+        }
+    }
+    public void SpawnMonsters() 
+    {
+        Transform spawnPoint = spawnPoint[Random.Range(0,spawnPoint.Length)];
+        Instantiate(EnemyPrefab, spawnPoint.position, spawnPoint.rotation);
+        currentSpwanCount++;
+    }**/
+}
