a15e748e89c73bb87f35f4b3075b92aad530fceb
yhc0607 <121930484+yhc0607@users.noreply.github.com>
2024-04-16 14:45:51 +0900

    아이템 에셋 추가

--- Assets/YHC/YHC_Scripts/Item/Test/Test_DropItem.cs
+++ Assets/YHC/YHC_Scripts/Item/Test/Test_DropItem.cs
@@ -2,23 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using static UnityEditor.PlayerSettings;
 
 public class Test_DropItem : TestBase
 {
-    GameObject obj;
-    MeshFilter meshFilter;
-    MeshRenderer meshRenderer;
-
-    private void Awake()
-    {
-        
-        obj = FindObjectOfType<ItemBase>().gameObject;
-        meshFilter = obj.GetComponent<MeshFilter>();
-        meshRenderer = obj.GetComponent<MeshRenderer>();
-    }
+    public Grenade grenade;
+    public CoilHead coilHead;
+    public Vector3 randPos;
 
     protected override void OnTest1(InputAction.CallbackContext context)
     {
+        coilHead.TracePlayer(randPos);
+    }
 
+    protected override void OnTest2(InputAction.CallbackContext context)
+    {
+        randPos = new Vector3(Random.Range(-30.0f, 30.0f), coilHead.transform.position.y, Random.Range(-30.0f, 30.0f));
+        Debug.Log($"{randPos}");
     }
 }
