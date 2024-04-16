e20073217217bef5324df3dffcc1affd4b7aeb24
DESKTOP-8900FC7\Atents <68528922+skyqkenr@users.noreply.github.com>
2024-04-16 14:40:11 +0900

    폐철물 스크립트에 IItemDataBase 인터페이스 상속
    폐철물 스크립트에 IItemDataBase 인터페이스 상속

--- Assets/KWS/_Script2/Hardware/Hardware_GarbageCart.cs
+++ Assets/KWS/_Script2/Hardware/Hardware_GarbageCart.cs
@@ -2,7 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Hardware_GarbageCart : ItemBase
+public class Hardware_GarbageCart : ItemBase, IItemDataBase
 {
     //public ItemDB itemDB;
+    public ItemDB GetItemDB()
+    {
+        throw new System.NotImplementedException();
+    }
 }
