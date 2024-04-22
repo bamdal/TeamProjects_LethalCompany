2001b6ca6cddb3e7631c5304122cb7970645f2c4
jms <alstn7695@gmail.com>
2024-04-22 14:56:14 +0900

    TestScene 수정

--- Assets/JMS/_Script/Test/Test_Modul.cs
+++ Assets/JMS/_Script/Test/Test_Modul.cs
@@ -9,19 +9,21 @@ public class Test_Modul : TestBase
     public Door Door;
     public GenerationPointNav generationPointNav;
 
-    protected override void OnTest1(InputAction.CallbackContext context)
-    {
-        Door.Test_OpenVector(target);
-    }
-
-    protected override void OnTest2(InputAction.CallbackContext context)
+    private void Start()
     {
         DungeonGenerator dungeonGenerator = FindAnyObjectByType<DungeonGenerator>();
         dungeonGenerator.StartGame();
 
     }
 
-    protected override void OnTest3(InputAction.CallbackContext context)
+    protected override void OnTest1(InputAction.CallbackContext context)
+    {
+        DungeonGenerator dungeonGenerator = FindAnyObjectByType<DungeonGenerator>();
+        dungeonGenerator.StartGame();
+
+    }
+
+    protected override void OnTest2(InputAction.CallbackContext context)
     {
         Transform[] child = generationPointNav.transform.GetComponentsInChildren<Transform>();
         foreach (Transform child2 in child)
