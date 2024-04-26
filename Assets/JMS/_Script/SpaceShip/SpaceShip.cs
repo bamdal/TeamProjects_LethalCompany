34413c225de2d29edac1aafea3b3e2cc5e676814
jms <alstn7695@gmail.com>
2024-04-26 15:20:48 +0900

    우주복귀 버튼 제작중

--- Assets/JMS/_Script/SpaceShip/SpaceShip.cs
+++ Assets/JMS/_Script/SpaceShip/SpaceShip.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,9 +7,21 @@ public class SpaceShip : MonoBehaviour
 {
     Animator animator;
 
+    /// <summary>
+    /// 함선의 있는 버튼(우주로 다시 돌아갈때 누르는 버튼)
+    /// </summary>
+    RedButton redButton;
+
     private void Awake()
     {
-        animator = GetComponent<Animator>();    
+        animator = GetComponent<Animator>();
+        redButton =GetComponentInChildren<RedButton>();
+        redButton.request += ButtonClick;
+    }
+
+    private void ButtonClick()
+    {
+        
     }
 
     private void Start()
