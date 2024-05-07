161ee1ea8a0c8647f7ca1bb4c791ca99e04f78b5
zion <sangwook4859@naver.com>
2024-05-07 13:31:49 +0900

    ?

--- Assets/SL/_Script/trap/trap.cs
+++ Assets/SL/_Script/trap/trap.cs
@@ -4,6 +4,25 @@ using UnityEngine;
 
 public class trap : MonoBehaviour
 {
+    TrapTrigger trapTrigger;
+    float currentY;
+    Coroutine raiseTrap = null;
+    private void Awake()
+    {
+        trapTrigger = GetComponentInParent<TrapTrigger>();
+        currentY = transform.position.y;
+    }
+
+    IEnumerator RaiseTrap()
+    {
+        while (transform.position.y < currentY)
+        {
+            float newY = transform.position.y + 1.0f * Time.deltaTime;
+            transform.position = new Vector3(transform.position.x, Mathf.Min(newY, currentY), transform.position.z);
+            yield return null;
+        }
+        trapTrigger.IsLowering = false;
+    }
     private void OnTriggerEnter(Collider other)
     {
         if(other.CompareTag("Player"))
