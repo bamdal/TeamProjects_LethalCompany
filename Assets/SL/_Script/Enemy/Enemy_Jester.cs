21101dc1469b5874ffaaf913e1bfe1564f858ecb
zion <sangwook4859@naver.com>
2024-05-10 14:37:29 +0900

    해골 추가중

--- Assets/SL/_Script/Enemy/Enemy_Jester.cs
+++ Assets/SL/_Script/Enemy/Enemy_Jester.cs
@@ -115,6 +115,7 @@ public class Enemy_Jester : EnemyBase
         if (changeTimer <= 0f)
         {
             changeTimer = changeModeTime;
+            transform.GetChild(2).gameObject.SetActive(false);
             State = EnemyState.Attack;
         }
     }
