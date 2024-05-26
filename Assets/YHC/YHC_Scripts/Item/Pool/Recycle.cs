b248d87e8d884e7b9d676fa384598321a64f85be
bamdal <alstn7695@gmail.com>
2024-05-26 20:39:34 +0900

    팩토리 버그 수정
    자식에 아이템 없어져서 에러 뜨고 있음

--- Assets/YHC/YHC_Scripts/Item/Pool/Recycle.cs
+++ Assets/YHC/YHC_Scripts/Item/Pool/Recycle.cs
@@ -7,6 +7,20 @@ public class Recycle : MonoBehaviour
 {
     public Action onDisable;
 
+    private void Start()
+    {
+        GameManager.Instance.onGameReady += OnDisableObject;
+    }
+
+    private void OnDisableObject()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Transform child = transform.GetChild(i);
+            child.gameObject.SetActive(false);
+        }
+    }
+
     protected virtual void OnDisable()
     {
         onDisable?.Invoke();    // 비활성화될때 큐에 되돌려야 하기때문에 델리게이트 실행해서 알리기
